cfe1edc67a1fe7a1b4ed47adf98e53cbf43ae3d5
Cullen Jett <cullenjett@gmail.com>
2018-04-01 11:07:06 -0600

    use Chokidar to detect/inject code changes instead of Nodemon and set up production build command

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A place to do Node API shit.
 ## Features
 
 * Typescript + Express API
+* Hot-reloading
 * Database TBD
 * Dockerized for local development
 
@@ -16,7 +17,7 @@ A place to do Node API shit.
 
 * `npm start`
 
-This starts a server on port 3000 that reloads on file changes.
+This starts a server on port 3000 with hot-reloading (ish?) thanks to [Chokidar](https://github.com/paulmillr/chokidar) and [this](https://codeburst.io/dont-use-nodemon-there-are-better-ways-fc016b50b45e) article.
 
 * `npm run docker` (requires Docker)
 
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import * as bodyParser from 'body-parser';
+import * as chokidar from 'chokidar';
 import * as express from 'express';
 import * as morgan from 'morgan';
 
-import { routes } from './routes';
+const watcher = chokidar.watch('./');
+
+watcher.on('ready', () => {
+  watcher.on('all', () => {
+    console.log('Reloading server...');
+    Object.keys(require.cache).forEach(id => {
+      if (/[\/\\]src[\/\\]/.test(id)) {
+        delete require.cache[id];
+      }
+    });
+  });
+});
 
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
-app.use(routes);
+
+app.use(async (req, res, next) => {
+  const { routes } = await import('./routes');
+  routes(req, res, next);
+});
 
 /* tslint:disable:no-console */
 app.listen(3000, () => console.log('App running on port 3000'));
